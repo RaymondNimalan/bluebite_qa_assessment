203c2cb18848641468953d3e6df6205e6002e55f
Ray Nimalan <raymondnimalan@gmail.com>
2023-06-17 13:01:37 -0400

    fix: update spec title

--- cypress/e2e/raffle.cy.js
+++ cypress/e2e/raffle.cy.js
@@ -1,4 +1,4 @@
-describe('Check if Input Field is Selected', () => {
+describe('Raffle Spec', () => {
   beforeEach(() => {
     cy.visit(
       'https://blue-bite-dev-3.bluebite.io/04425f7c-4fdd-47f6-85b3-b800d12bb9ca'
