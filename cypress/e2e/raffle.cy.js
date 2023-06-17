c9972f3a08811ae0b1634fd5d98b8e19952c0e26
Ray Nimalan <raymondnimalan@gmail.com>
2023-06-17 00:37:29 -0400

    feat: test case 1

--- cypress/e2e/raffle.cy.js
+++ cypress/e2e/raffle.cy.js
@@ -1,26 +1,17 @@
-describe('Raffle Entry', () => {
+describe('Check if Input Field is Selected', () => {
   beforeEach(() => {
     cy.visit(
       'https://blue-bite-dev-3.bluebite.io/04425f7c-4fdd-47f6-85b3-b800d12bb9ca'
     );
+  });
 
-    it('Customer enters the raffle with all required information and a reason', () => {
-      cy.get('#input-3').type('John Doe');
-      cy.get('#input-4').type('john.doe@example.com');
-      cy.get('#input-8').type('40');
-      cy.get('#input-9').type('I should win');
-      cy.get('.button-sc-1ta45yz-0').click();
-      cy.contains('Submission Confirmed');
-      cy.contains('You have registed 1 submission');
-    });
-
-    it('Customer enters the raffle with all required information and without a reason ', () => {
-      cy.get('#input-3').type('John Doe');
-      cy.get('#input-4').type('john.doe@example.com');
-      cy.get('#input-8').type('40');
-      cy.get('.button-sc-1ta45yz-0').click();
-      cy.contains('Submission Confirmed');
-      cy.contains('You have registed 1 submission');
-    });
+  it('Customer enters the raffle with all required information and a reason', () => {
+    cy.get('#input-3').type('John Doe');
+    cy.get('#input-4').type('john.doe@example.com');
+    cy.get('#input-8').type('40');
+    cy.get('#input-9').type('I should win');
+    cy.get('.button-sc-1ta45yz-0').click();
+    cy.contains('Submission Confirmed');
+    cy.contains('You have registed 1 submission');
   });
 });
