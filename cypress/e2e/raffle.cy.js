f40dab99ae7bde42413267fb7af14d6f33738386
Ray Nimalan <raymondnimalan@gmail.com>
2023-06-17 00:39:34 -0400

    feat: test case 3

--- cypress/e2e/raffle.cy.js
+++ cypress/e2e/raffle.cy.js
@@ -23,4 +23,22 @@ describe('Check if Input Field is Selected', () => {
     cy.contains('Submission Confirmed');
     cy.contains('You have registed 1 submission');
   });
+
+  it('Customer enters the raffle multiple times', () => {
+    cy.get('#input-3').type('John Doe');
+    cy.get('#input-4').type('john.doe@example.com');
+    cy.get('#input-8').type('40');
+    cy.get('#input-9').type('I should win');
+    cy.get('.button-sc-1ta45yz-0').click();
+    cy.contains('Submission Confirmed');
+    cy.contains('You have registed 1 submission');
+    cy.reload();
+    cy.get('#input-3').type('John Doe');
+    cy.get('#input-4').type('john.doe@example.com');
+    cy.get('#input-8').type('40');
+    cy.get('#input-9').type('I should win');
+    cy.get('.button-sc-1ta45yz-0').click();
+    cy.contains('Submission Confirmed');
+    cy.contains('You have registed 2 submission');
+  });
 });
