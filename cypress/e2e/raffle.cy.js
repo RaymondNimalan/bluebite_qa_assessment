cf993665e2f3496bc7df2f475933c668310613ee
Ray Nimalan <raymondnimalan@gmail.com>
2023-06-17 12:53:14 -0400

    feat: test case 9

--- cypress/e2e/raffle.cy.js
+++ cypress/e2e/raffle.cy.js
@@ -90,4 +90,13 @@ describe('Check if Input Field is Selected', () => {
     cy.get('.button-sc-1ta45yz-0').click();
     cy.contains('Must be 18 or older to enter.');
   });
+
+  it('Customer enters the raffle with an invalid email', () => {
+    cy.get('#input-3').type('John Doe');
+    cy.get('#input-8').type('40');
+    cy.get('#input-4').type('john.do');
+    cy.get('#input-9').type('I should win');
+    cy.get('.button-sc-1ta45yz-0').click();
+    cy.get('.button-sc-1ta45yz-0').contains('Submit');
+  });
 });
